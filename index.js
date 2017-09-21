266b187da7538861bf021b5223ba030fd5c2a22e
joey <karpuch13@yahoo.com>
2017-09-21 15:42:24 +0300

    feat: basic layout

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { DatePicker } from 'antd';
+import { Layout, Input, Icon } from 'antd';
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div style={{ margin: 100 }}>
-      <h1>AntDesign Demo</h1>
-      <hr /><br />
-      <DatePicker />
-    </div>
+    <Layout className="app__layout">
+      <Header className="app__header">
+        <div className="app__logo" />
+        <h1>Transport API</h1>
+      </Header>
+      <Content className="app__wrapper">
+        <div className="app__content">
+          <Input suffix={<Icon type="search" />} placeholder="Please, enter the location" />
+        </div>
+      </Content>
+      <Footer className="app__footer">
+        Created by Andery Karpenko - 2017
+      </Footer>
+    </Layout>
   );
 }
 
